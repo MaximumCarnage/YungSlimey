cf7511b4e500c5e6e413a15c47fa8e89f171cf58
MaximumCarnage <dylansbruton@gmail.com>
2017-11-24 14:36:52 -0500

    Current Working Astralform

--- Assets/Scripts/CharacterChanging.cs
+++ Assets/Scripts/CharacterChanging.cs
@@ -13,27 +13,30 @@ public class CharacterChanging : MonoBehaviour {
 	public characterType m_currentType;
 	public AnimatorController m_normalAnim; 
 	public AnimatorController m_astralAnim; 
+	public Camera m_mainCam;
+	public ThirdPersonOrbitCamBasic m_camScript;
 	void Start(){
 		m_currentType = characterType.NORMAL;
 		m_playeranimator = GetComponent<Animator>();
+		m_camScript = m_mainCam.GetComponent<ThirdPersonOrbitCamBasic>();
 	}
 
 	void Update(){
 		
 		if(Input.GetKeyDown(KeyCode.O) && m_currentType == characterType.NORMAL){
-			if(m_currentType == characterType.NORMAL){
 				m_currentType = characterType.ASTRAL;
-				Instantiate(m_sleepyPlayer,transform.position,Quaternion.identity);
-				
+				Instantiate(m_sleepyPlayer,m_currentAvatar.position,Quaternion.identity);
+				m_camScript.player=m_astralAvatar.transform;
 				
 				 m_normalAvatar.SetActive(false);
 				 m_astralAvatar.SetActive(true);
-			}
+			
 			
 			
 		}
 		else if(Input.GetKeyDown(KeyCode.O) && m_currentType == characterType.ASTRAL){
 				m_currentType = characterType.NORMAL;
+				m_camScript.player=m_normalAvatar.transform;
 				 m_astralAvatar.SetActive(false);
 				 m_normalAvatar.SetActive(true);
 			}
