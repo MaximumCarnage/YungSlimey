a8dcb0128f374c9c62971a2f101a938ebb0529e2
MaximumCarnage <dylansbruton@gmail.com>
2017-11-24 20:05:39 -0500

    tiger in-progress

--- Assets/Scripts/CharacterChanging.cs
+++ Assets/Scripts/CharacterChanging.cs
@@ -6,8 +6,8 @@ public enum characterType{ NORMAL, ASTRAL, BIG , SMALL , FLIGHT};
 
 public class CharacterChanging : MonoBehaviour {
 	public Animator m_playeranimator;
-	public GameObject m_astralAvatar;
-	public GameObject m_normalAvatar;
+
+	public GameObject[] m_playerTypes;
 	public GameObject m_sleepyPlayer;
 	public Transform  m_currentAvatar;
 	public characterType m_currentType;
@@ -26,20 +26,22 @@ public class CharacterChanging : MonoBehaviour {
 		if(Input.GetKeyDown(KeyCode.O) && m_currentType == characterType.NORMAL){
 				m_currentType = characterType.ASTRAL;
 				Instantiate(m_sleepyPlayer,m_currentAvatar.position,Quaternion.identity);
-				m_camScript.player=m_astralAvatar.transform;
-				m_astralAvatar.transform.position=m_currentAvatar.position;
-				m_currentAvatar=m_astralAvatar.transform;
-				 m_normalAvatar.SetActive(false);
-				 m_astralAvatar.SetActive(true);
+				m_camScript.player=m_playerTypes[1].transform;
+				m_playerTypes[1].transform.position=m_currentAvatar.position;
+				m_currentAvatar=m_playerTypes[1].transform;
+				 m_playerTypes[0].SetActive(false);
+				 m_playerTypes[1].SetActive(true);
 			
 			
 			
 		}
 		else if(Input.GetKeyDown(KeyCode.O) && m_currentType == characterType.ASTRAL){
 				m_currentType = characterType.NORMAL;
-				m_camScript.player=m_normalAvatar.transform;
-				 m_astralAvatar.SetActive(false);
-				 m_normalAvatar.SetActive(true);
+				m_camScript.player=m_playerTypes[0].transform;
+				m_playerTypes[0].transform.position=m_currentAvatar.position;
+				m_currentAvatar=m_playerTypes[0].transform;
+				 m_playerTypes[1].SetActive(false);
+				 m_playerTypes[0].SetActive(true);
 			}
 	}
 }
