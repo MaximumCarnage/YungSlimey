ec7ea59fb840f1da005012201c3f1927e5b8af7d
MaximumCarnage <dylansbruton@gmail.com>
2017-11-28 21:30:28 -0500

    attack/health setup

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+	public int m_health = 100;
+	public Animator m_anim;
+	// Use this for initialization
+	void Start () {
+		m_anim=gameObject.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log(m_health);
+	}
+	public void takeDamage(int damage){
+		m_health -= damage;
+	}
+
+}
---
+++ Assets/Scripts/SwordStrike.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordStrike : MonoBehaviour {
+
+	void OnTriggerEnter(Collision other){
+		if(other.gameObject.tag == "Enemy"){
+			other.gameObject.GetComponent<Health>();
+		}
+	}
+}
