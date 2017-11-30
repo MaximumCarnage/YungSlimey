a78abfef4419350c84d69653ba7c9d740ba8f554
MaximumCarnage <dylansbruton@gmail.com>
2017-11-30 05:42:38 -0500

    StartingAI

---
+++ Assets/Scripts/MoveAI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveAI : MonoBehaviour {
+	public Transform goal;
+	private NavMeshAgent agent;
+	private Animator m_anim;
+	// Use this for initialization
+	void Start () {
+		agent = gameObject.GetComponent<NavMeshAgent>();
+		m_anim = gameObject.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		agent.updateRotation = true;
+		if(goal){
+			agent.SetDestination(goal.position);
+			
+		}
+		
+	}
+}
