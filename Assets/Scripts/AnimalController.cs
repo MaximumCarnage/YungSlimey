b908889127d6b82308dc31f4f64d707937b9f020
MaximumCarnage <dylansbruton@gmail.com>
2017-11-30 21:25:18 -0500

    Wander Behaviors

---
+++ Assets/Scripts/AnimalController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimalController : MonoBehaviour {
+
+   public GameObject player;
+ 
+     // Direction of movement
+     Vector3 speedUp;
+     Vector3 speedDown;
+     Vector3 speedLeft;
+     Vector3 speedRight;
+ 
+     // For making code easier to read
+     Rigidbody playerGetRigidbody;
+ 
+     float playerSpeed = 3.0f;
+     
+     void Start () {
+         playerGetRigidbody = player.GetComponent<Rigidbody>();
+         
+         speedUp    = new Vector3( 0, 0, 3);
+         speedDown  = new Vector3( 0, 0,-3);
+         speedLeft  = new Vector3(-3, 0, 0);
+         speedRight = new Vector3( 3, 0, 0);
+     }
+     
+     void Update () {
+	 }
+}
---
+++ Assets/Scripts/WanderAI.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class WanderAI : MonoBehaviour {
+	public float wanderRadius;
+    public float wanderTimer;
+ 
+    private Transform target;
+    private NavMeshAgent agent;
+    private float timer;
+  void Start () {
+        agent = GetComponent<NavMeshAgent> ();
+        timer = wanderTimer;
+		agent.updateUpAxis = false;
+    }
+ 
+    // Update is called once per frame
+    void Update () {
+        timer += Time.deltaTime;
+ 
+        if (timer >= wanderTimer) {
+            Vector3 newPos = RandomNavSphere(transform.position, wanderRadius, -1);
+            agent.SetDestination(newPos);
+
+            timer = 0;
+			
+        }
+		
+    }
+ 
+    public static Vector3 RandomNavSphere(Vector3 origin, float dist, int layermask) {
+        Vector3 randDirection = Random.insideUnitSphere * dist;
+		
+        randDirection += origin;
+ 
+        NavMeshHit navHit;
+ 
+        NavMesh.SamplePosition (randDirection, out navHit, dist, layermask);
+ 
+        return navHit.position;
+    }
+	void OnTriggerStay(Collider other){
+		Debug.Log("Aggro");
+		if(other.gameObject.tag == "PlayerBody"){
+			agent.SetDestination(other.gameObject.transform.position);
+		}
+	}
+	void OnTriggerExit(Collider other){
+		Vector3 newPos = RandomNavSphere(transform.position, wanderRadius, -1);
+            agent.SetDestination(newPos);
+
+	}
+}
